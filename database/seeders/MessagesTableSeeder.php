dece7ce1ddc81314af1fb7f02f8f37e31f473809
baku.shoda <baku-shoda@ielove-group.jp>
2024-07-27 22:23:38 +0900

    add seeder

---
+++ database/seeders/MessagesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MessagesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        for($i = 1; $i <= 100; $i++) {
+            DB::table('messages')->insert([
+                'title' => 'test title ' . $i,
+                'content' => 'test content ' . $i
+            ]);
+        }
+    }
+}
